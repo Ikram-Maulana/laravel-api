61ee3756bef599118575d9b21e96c77cedbb74c7
Ikram Maulana <ikram075@ummi.ac.id>
2022-11-24 19:06:11 +0700

    Feat: Create RESTful API Login
    - Membuat Controller Login
    - Menambahkan Route Login dan Get User
    - Uji Coba API Login

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -16,8 +16,23 @@ use Illuminate\Support\Facades\Route;
 
 // Posts
 Route::apiResource('/posts', App\Http\Controllers\Api\PostController::class);
+
 /**
  * route "/register"
  * @method "POST"
  */
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+
+/**
+ * route "/login"
+ * @method "POST"
+ */
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+
+/**
+ * route "/user"
+ * @method "GET"
+ */
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+  return $request->user();
+});
